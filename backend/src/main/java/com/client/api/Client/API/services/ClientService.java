39411eb326173fe0648b72f8d2b6697c4473ceb5
supertz4r <juninsoader@gmail.com>
2023-12-26 14:06:18 -0300

    Get all clients

--- backend/src/main/java/com/client/api/Client/API/services/ClientService.java
+++ backend/src/main/java/com/client/api/Client/API/services/ClientService.java
@@ -1,8 +1,24 @@
 package com.client.api.Client.API.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.client.api.Client.API.dto.ClientDTO;
+import com.client.api.Client.API.entities.Client;
+import com.client.api.Client.API.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(c -> new ClientDTO(c));
+	}
 }
