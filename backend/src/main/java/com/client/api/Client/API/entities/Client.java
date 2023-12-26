034379b71eaea56ff6351b3b8f5ff7e7849a1c8b
supertz4r <juninsoader@gmail.com>
2023-12-26 13:44:18 -0300

    implements ClientDTO

--- backend/src/main/java/com/client/api/Client/API/entities/Client.java
+++ backend/src/main/java/com/client/api/Client/API/entities/Client.java
@@ -26,6 +26,23 @@ public class Client implements Serializable{
 	
 	public Client() {}
 
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
--- backend/src/main/java/com/client/api/Client/API/repositories/ClientRepository.java
+++ backend/src/main/java/com/client/api/Client/API/repositories/ClientRepository.java
@@ -1,8 +1,10 @@
 package com.client.api.Client.API.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ClientRepository {
+import com.client.api.Client.API.entities.Client;
 
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
 }
